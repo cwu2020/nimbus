221ca4b6c70be9cc0887e7e46d6b786d4e4e7b07
oanaciocioman <oana.ciocioman@gmail.com>
2018-09-15 19:20:08 -0400

    On click submit print the value of the task. Added a new type of button: In use.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,23 @@ import './App.css';
 
 class App extends Component {
 
+   constructor(props) {
+       super(props);
+       this.state = {
+           task: ""
+       }
+   }
+
+   renderButton(type) {
+       if (type === 'Available') {
+           return <button type="button" class="btn btn-success" style={{marginLeft: "60px", marginTop: "20px"}}>Available</button>;
+       } else if (type === 'Idle') {
+           return <button type="button" class="btn btn-secondary btn-lg" style={{marginLeft: "60px", marginTop: "20px"}}>Idle</button>;
+       } else {
+           return <button type="button" class="btn btn-primary" style={{marginLeft: "60px", marginTop: "20px"}}>In Use</button>
+       }
+   }
+
   renderDevice(data) {
       return (
           <div className="display-flex" style={{marginTop: "3%"}}>
@@ -10,7 +27,7 @@ class App extends Component {
                   <div className="card-body no-padding">
                       <div className="display-flex">
                           <div className="flex-value">
-                              <img src="https://media.wired.com/photos/5b22c5c4b878a15e9ce80d92/master/pass/iphonex-TA.jpg" width="80" height="80"/>
+                              <img src="https://media.wired.com/photos/5b22c5c4b878a15e9ce80d92/master/pass/iphonex-TA.jpg" width="90" height="80"/>
                           </div>
                           <div className="flex-value device-specification">
                               Device: {data.name}
@@ -21,11 +38,18 @@ class App extends Component {
                   </div>
               </div>
               <div className="flex-value">
-                  {data.isAvailable ? <button type="button" class="btn btn-lg btn-primary" style={{marginLeft: "60px", marginTop: "20px"}}>Available</button> :
-                      <button type="button" class="btn btn-secondary btn-lg">Idle</button>}
+                  {this.renderButton(data.type)}
               </div>
           </div>
       );
+  }
+
+  onChangeTextarea(event) {
+       this.setState({task: event.target.value});
+  }
+
+  onClickSubmit() {
+       console.log(this.state.task);
   }
 
   render() {
@@ -39,16 +63,16 @@ class App extends Component {
                     <div className="card-height tasks-card-height">
                         <div class="form-group">
                             <label className="upload-task-title">Upload Task</label>
-                            <textarea className="form-control code-height"></textarea>
+                            <textarea className="form-control code-height" value={this.state.task} onChange={this.onChangeTextarea.bind(this)}></textarea>
                             <br />
-                            <button type="button" class="btn btn-primary">Submit task</button>
+                            <button type="button" class="btn btn-primary" onClick={this.onClickSubmit.bind(this)}>Submit task</button>
                         </div>
                     </div>
                 </div>
                 <div class="right-half">
                     <div className="devices-card-height">
                         <label className="upload-task-title">Devices Registered</label>
-                        {this.renderDevice({isAvailable: true, name: "iPhone X", software: "iOS 11"})}
+                        {this.renderDevice({type: 'In Use', name: "iPhone X", software: "iOS 11"})}
                     </div>
                 </div>
             </section>
