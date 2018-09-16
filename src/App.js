b4ffcf6fbe52336fc9018b99139a9b5a2ca7c530
oanaciocioman <oana.ciocioman@gmail.com>
2018-09-16 14:19:40 -0400

    Update the status of the registered devices to available / in use

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import {fetch} from "whatwg-fetch";
+import request from "superagent";
+let response = "";
 
 class App extends Component {
 
@@ -10,9 +12,36 @@ class App extends Component {
            task: "",
            queue: [],
            time: "Short",
+           inUseDevices: [],
+           availableIndex: 0,
+           iphone1: false,
+           iphone2: false,
+           iphone3: false,
+           iphone4: false,
+           iphone5: false,
+           iphone6: false,
+           d: [],
+           t: [],
+           de: null
+
        }
-       this.onClickSubmit.bind(this);
+
+       setInterval(() => {
+           request
+               .post('https://85bc5698.ngrok.io/compute/stats')
+               .send({}) // sends a JSON post body
+               .set("content-type", "application/x-www-form-urlencoded")
+               .set("accept", "application/json")
+               .end((err, res) => {
+                   const response = JSON.parse(res.text);
+                   this.setState({d: response.d, t: response.t})
+                   // Calling the end function will send the request
+               });
+       }, 5000);
+
+       this.onClickSubmit = this.onClickSubmit.bind(this);
    }
+
 
    renderButton(type) {
        if (type === 'Available') {
@@ -23,6 +52,25 @@ class App extends Component {
            return <button type="button" className="btn btn-primary" style={{marginLeft: "60px", marginTop: "20px"}}>In Use</button>
        }
    }
+
+   // componentDidMount() {
+   //     setInterval(() => {
+   //         var data = null;
+   //
+   //         var xhr = new XMLHttpRequest();
+   //         xhr.withCredentials = true;
+   //
+   //         xhr.onreadystatechange = () => {
+   //             this.setState({de: this.responseText})
+   //         };
+   //
+   //         xhr.open("POST", "https://b78a31e4.ngrok.io/compute/stats");
+   //         xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+   //         xhr.setRequestHeader("accept", "application/json");
+   //         xhr.setRequestHeader("cache-control", "no-cache");
+   //         xhr.send(JSON.stringify(data));
+   //     }, 1000)
+   // }
 
   renderDevice(data) {
       return (
@@ -56,11 +104,12 @@ class App extends Component {
         this.setState({time: event.target.value});
   }
 
-  onClickSubmit() {
+  onClickSubmit = () => {
       const task = this.state.task;
       const newQueue = this.state.queue.concat([task]);
       console.log(newQueue);
       this.setState({queue: newQueue, task: ""});
+
       var data = {algo: task, time: this.state.time};
 
       var xhr = new XMLHttpRequest();
@@ -68,18 +117,20 @@ class App extends Component {
 
       xhr.addEventListener("readystatechange", function () {
           if (this.readyState === 4) {
-              console.log(this.responseText);
+             response = JSON.parse(this.responseText)["index"];
           }
       });
 
-      xhr.open("POST", "https://1142a365.ngrok.io/compute/add/task");
+      xhr.open("POST", "https://85bc5698.ngrok.io/compute/add/task");
       xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
       xhr.setRequestHeader("accept", "application/json");
       xhr.setRequestHeader("cache-control", "no-cache");
       xhr.send(JSON.stringify(data));
-   }
+
+  }
 
   render() {
+      console.log(this.state.d);
       return (
           <div className="App">
             <header className="App-header">
@@ -107,7 +158,13 @@ class App extends Component {
                 <div className="right-half">
                     <div className="devices-card-height">
                         <label className="upload-task-title">Devices Registered</label>
-                        {this.renderDevice({type: 'In Use', name: "iPhone X", software: "iOS 11"})}
+                        {this.state.d.map((item, i) => {
+                            let type = 'Available';
+                            if(this.state.d[i].task !== -1) {
+                                type = 'In Use'
+                            }
+                            return this.renderDevice({type: type, name: (this.state.d[i]).model, software: (this.state.d[i]).OS})
+                        })}
                     </div>
                 </div>
             </section>
